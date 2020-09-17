51976c322663e9e54e364b0e3448e767a5a521f6
Robbie <stillrobbie@gmail.com>
2020-09-17 23:15:46 +0100

    delete hello.R

--- R/pitch_us.R
+++ R/pitch_us.R
@@ -1,14 +1,17 @@
-#' Plot a fill-less half pitch for an understat shotmap. 
-#' 
-#' @param filler the colour you want your lines to be. 
-#' @return a ggplot annotation half pitch. 
+#' Plot a transparent half pitch for an understat shotmap.
+#'
+#' @param filler the colour you want your pitch lines to be. there is no
+#' background colour option, please use plot.background and panel.background. this
+#' is so the pitch can be plotted over heatmaps.
+#'
+#' @return a ggplot annotation half pitch.
 #' @examples
 #' library(understatr)
 #' library(ggplot2)
-#' 
-#' data <- get_player_shots(629) 
 #'
-#' ggplot(data = data, aes(x = X, y = Y, size = xG, 
+#' data <- get_player_shots(629)
+#'
+#' ggplot(data = data, aes(x = X, y = Y, size = xG,
 #'                         colour = result,
 #'                         shape = shotType, fill = result),
 #'        alpha = 0.7) +
@@ -17,10 +20,10 @@
 
 pitch_us <- function(filler = "#2f4f4f"){list(#half pitch
 #right touchline
-annotate("segment",x = 0.6, xend = 1, y = 0, yend = 0, colour = filler, 
+annotate("segment",x = 0.6, xend = 1, y = 0, yend = 0, colour = filler,
          size = 0.6) ,
   #left touchline
-  annotate("segment",x = 0.6, xend = 1, y = 1, yend = 1, colour = filler, 
+  annotate("segment",x = 0.6, xend = 1, y = 1, yend = 1, colour = filler,
            size = 0.6) ,
   #goal line
   annotate("segment", x = 1, xend = 1, y = 0, yend = 1, colour = filler,
@@ -37,14 +40,14 @@ annotate("segment",x = 0.6, xend = 1, y = 0, yend = 0, colour = filler,
   theme(rect = element_blank(),
         line = element_blank()) ,
   annotate("point", x = 0.885 , y = 0.500, colour = filler, size = 1.05) ,
-  annotate("path", x=0.885-0.091*cos(seq(-0.295*pi,0.295*pi,length.out=30)), 
+  annotate("path", x=0.885-0.091*cos(seq(-0.295*pi,0.295*pi,length.out=30)),
            size = 0.6,
            y=0.498+0.116*sin(seq(-0.3*pi,0.3*pi,length.out=30)), col=filler) ,
-  coord_flip(), 
+  coord_flip(),
   xlim(0.6, 1.01),
   ylim(1,0),
-  theme(axis.line = element_blank(), 
-        axis.text = element_blank(), 
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
         axis.title = element_blank()))
   }
 
